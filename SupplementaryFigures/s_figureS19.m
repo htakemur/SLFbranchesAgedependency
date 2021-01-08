040eaf798cdd8dc244ee8b4441bff4d47090ff3b
htakemur <htakemura0207@gmail.com>
2021-01-08 10:04:53 +0900

    Include script for Figure S19 and minor fix on script for Figure 2

--- SupplementaryFigures/s_figureS19.m
+++ SupplementaryFigures/s_figureS19.m
@@ -1,18 +1,19 @@
-function s_figure2
+function s_figureS19
 
-% Plot mean and standard deviation of volumes of SLF I, II, and III in each age group. This script aims to reproduce Figure 2 in a following article:
+% Plot mean and standard deviation of volumes of SLF I, II, and III in each age group, estimated by using exclusive ROIs.
+% This script aims to reproduce Supplementary Figure 19 in a following article:
 %
 % Amemiya, K., Naito, E. & Takemura, H. (2021)
 %  Age dependency and lateralization in the three branches of the human superior longitudinal fasciculus. In revision.
 %
 % Hiromasa Takemura, NICT CiNet BIT
 
-cd Data/TractVolume_Main/
+cd ../Data/TractVolume_ExclusiveROI/
 
-FileToLoad{1}='CH_tractvolume_main.mat';
-FileToLoad{2}='ADO_tractvolume_main.mat';
-FileToLoad{3}='ADU_tractvolume_main.mat';
-FileToLoad{4}='SEN_tractvolume_main.mat';
+FileToLoad{1}='CH_tractvolume_exclusiveROI.mat';
+FileToLoad{2}='ADO_tractvolume_exclusiveROI.mat';
+FileToLoad{3}='ADU_tractvolume_exclusiveROI.mat';
+FileToLoad{4}='SEN_tractvolume_exclusiveROI.mat';
 
 % Load data and comppute mean/standard error
 for i = 1:4
@@ -27,7 +28,7 @@ groupnames = {'Left SLF I', 'Left SLF II', 'Left SLF III', 'Right SLF I', 'Right
 ordernum = [1 4 2 5 3 6];
 
 % Plot results
-for ng = 1:6;
+for ng = 1:6
     subplot (3,2,ng)
     h = bar(transpose(tractvolume_mean(ordernum(ng),:)));
     h(1).FaceColor = [0.2078 0.1647 0.5255];
@@ -35,7 +36,7 @@ for ng = 1:6;
     er = errorbar(transpose(tractvolume_mean(ordernum(ng),:)),transpose(tractvolume_ser(ordernum(ng),:)));
     er.Color = 'red';
     er.LineStyle = 'none';
-    ylim([0 20000])
+    ylim([0 20000]);
     yticks([0 10000 20000]);
     ylabel('Tract volume (mm^3)');
     set(gca, 'tickdir', 'out', 'box', 'off');
