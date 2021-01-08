ef21133214619ad352b62f9fe00abf167763849a
htakemur <htakemura0207@gmail.com>
2021-01-08 10:12:30 +0900

    Add script for Figure S20 and minor fix on script for Figure 3

--- SupplementaryFigures/s_figureS20.m
+++ SupplementaryFigures/s_figureS20.m
@@ -1,18 +1,19 @@
-function s_figure3
+function s_figureS20
 
-% Plot mean and standard deviation of fractional anistropy (FA) along SLF I, II, and III in each age group. This script aims to reproduce Figure 3 in a following article:
+% Plot mean and standard deviation of fractional anistropy (FA) along SLF I, II, and III in each age group estimated by using exclusive ROIs.
+% This script aims to reproduce Supplementary Figure 20 in a following article:
 %
 % Amemiya, K., Naito, E. & Takemura, H. (2021)
 %  Age dependency and lateralization in the three branches of the human superior longitudinal fasciculus. In revision.
 %
 % Hiromasa Takemura, NICT CiNet BIT
 
-cd Data/FAqR1_Main/
+cd ../Data/FAqR1_ExclusiveROI/
 
-FileToLoad{1}='CH_FAqR1_main.mat';
-FileToLoad{2}='ADO_FAqR1_main.mat';
-FileToLoad{3}='ADU_FAqR1_main.mat';
-FileToLoad{4}='SEN_FAqR1_main.mat';
+FileToLoad{1}='CH_FAqR1_exclusiveROI.mat';
+FileToLoad{2}='ADO_FAqR1_exclusiveROI.mat';
+FileToLoad{3}='ADU_FAqR1_exclusiveROI.mat';
+FileToLoad{4}='SEN_FAqR1_exclusiveROI.mat';
 
 % Load data and comppute mean/standard error
 for i = 1:4
